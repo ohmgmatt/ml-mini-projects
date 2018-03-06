860ccbbb8faefd71dcf80baccd16a7ab0bcf8132
ohmgmatt <bonilla.matthew44@gmail.com>
2018-03-06 07:56:59 -0800

    Added DT and finding features importances

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -2,13 +2,15 @@
 
 import pickle
 import numpy
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 numpy.random.seed(42)
 
 
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
+words_file = "../text_learning/your_word_data.pkl"
 authors_file = "../text_learning/your_email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
@@ -39,5 +41,9 @@ labels_train   = labels_train[:150]
 
 ### your code goes here
 
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
+print accuracy_score(pred, labels_test)
+print clf.feature_importances_
