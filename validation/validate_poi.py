f2487dcda7103242976f0457196dcd28ea1096e6
ohmgmatt <bonilla.matthew44@gmail.com>
2018-03-12 08:23:41 -0700

    Introducting DT and train test split

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,6 +14,7 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import cross_validation
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -22,11 +23,27 @@ data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") 
 ### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
+
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
 
-### it's all yours from here forward!  
+### it's all yours from here forward!
+
+## Split Data into train/test
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(
+features, labels, test_size = .3, random_state = 42
+)
 
 
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+print(acc)
